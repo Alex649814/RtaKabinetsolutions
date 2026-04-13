a1b7b2b7dbc9077fee3e8c5c16596c38c354bacc
Alex649814 <L21330388@hermosillo.tecnm.mx>
2026-04-13 15:49:07 -0700

    Cambio de logo

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -58,9 +58,11 @@ const NavigationBar = () => {
         >
           <div className="flex items-center">
             <Link to="/" className="flex items-center">
-              <h1 className="text-3xl sm:text-4xl font-extrabold text-black tracking-tight mr-6">
-                RTA KABINETS
-              </h1>
+              <img
+                src={logo}
+                alt="RTA Kabinets Solutions Logo"
+                className="h-14 sm:h-16 w-auto mr-6 object-contain"
+              />
             </Link>
           </div>
 
